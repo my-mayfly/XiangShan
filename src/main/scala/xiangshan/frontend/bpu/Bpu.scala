60a8c587856adfceb23962da0244439cec3ce16d
zhou tao <mayflyandtree@163.com>
2025-11-29 00:28:35 +0800

    feat(utage): add performance counter

--- src/main/scala/xiangshan/frontend/bpu/Bpu.scala
+++ src/main/scala/xiangshan/frontend/bpu/Bpu.scala
@@ -291,13 +291,6 @@ class Bpu(implicit p: Parameters) extends BpuModule with HalfAlignHelper {
   // TODO: maybe need compare position？
 
   // When microTAGE participates in prediction, it has the highest priority in stage S1.
-  // private val shouldUseUtage =
-  //   (ubtb.io.prediction.cfiPosition === utage.io.prediction.bits.cfiPosition) && utage.io.prediction.valid
-  // private val s1_realUbtbTaken = Mux(
-  //   shouldUseUtage,
-  //   utage.io.prediction.bits.taken,
-  //   ubtb.io.prediction.taken && !abtb.io.useMicroTage
-  // )
   private val s1_realUbtbTaken = ubtb.io.prediction.taken && !abtb.io.useMicroTage
   s1_prediction :=
     MuxCase(
@@ -312,17 +305,18 @@ class Bpu(implicit p: Parameters) extends BpuModule with HalfAlignHelper {
   private val baseBrTaken = Mux(
     ubtb.io.prediction.taken,
     ubtb.io.prediction.attribute.isConditional,
-    Mux(abtb.io.prediction.taken, abtb.io.prediction.attribute.isConditional, false.B)
+    Mux(abtb.io.basePrediction.taken, abtb.io.basePrediction.attribute.isConditional, false.B)
   )
   private val baseBrCfiPosition = Mux(
     ubtb.io.prediction.taken,
     ubtb.io.prediction.cfiPosition,
-    Mux(abtb.io.prediction.taken, abtb.io.prediction.cfiPosition, 0.U)
+    Mux(abtb.io.basePrediction.taken, abtb.io.basePrediction.cfiPosition, 0.U)
   )
 
-  s1_utageMeta                 := utage.io.meta.bits
-  s1_utageMeta.baseTaken       := baseBrTaken
-  s1_utageMeta.baseCfiPosition := baseBrCfiPosition
+  s1_utageMeta                  := utage.io.meta.bits
+  s1_utageMeta.testUseMicroTage := abtb.io.useMicroTage
+  s1_utageMeta.baseTaken        := baseBrTaken
+  s1_utageMeta.baseCfiPosition  := baseBrCfiPosition
 
   private val s2_mbtbResult    = mbtb.io.result
   private val s2_condTakenMask = tage.io.condTakenMask
